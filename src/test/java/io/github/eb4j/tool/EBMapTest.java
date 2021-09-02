b167f861683109822388beae4b9d445b8696e1c5
Hiroshi Miura <miurahr@linux.com>
2021-09-02 16:40:50 +0900

    test: Update EBMapTest
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- src/test/java/io/github/eb4j/tool/EBMapTest.java
+++ src/test/java/io/github/eb4j/tool/EBMapTest.java
@@ -3,11 +3,15 @@ package io.github.eb4j.tool;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.nio.file.Files;
 import java.util.Objects;
 
 import io.github.eb4j.ext.UnicodeMap;
 import io.github.eb4j.tool.appendix.AltDef;
 import io.github.eb4j.tool.appendix.Appendix;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
 
 public class EBMapTest {
 
@@ -18,15 +22,31 @@ public class EBMapTest {
         EBMap ebMap = new EBMap();
         UnicodeMap unicodeMap = new UnicodeMap(mapPath);
         Appendix appendix = ebMap.constructData(unicodeMap);
+        assertNotNull(appendix);
     }
 
     @Test
-    public void testEBMapCreateGenerateAltDef() throws Exception {
+    public void testEBMapLoadAltDef() throws Exception {
         File mapPath = new File(Objects.requireNonNull(this.getClass().getResource("/data/chimei.map"))
                 .getFile()).getAbsoluteFile();
         EBMap ebMap = new EBMap();
         UnicodeMap unicodeMap = new UnicodeMap(mapPath);
         AltDef narrow = ebMap.generateAltDef(unicodeMap.getNarrowMap());
+        assertEquals(narrow.getStart(), 0xA121);
+        assertEquals(narrow.getEnd(), 0xA222);
+        assertEquals(narrow.getAlt(0xA12D), "a");
+    }
+
+    @Test
+    public void testEBMapCreateYaml() throws Exception {
+        File mapPath = new File(Objects.requireNonNull(this.getClass().getResource("/data/chimei.map"))
+                .getFile()).getAbsoluteFile();
+        File outPath = new File(Files.createTempDirectory("testEBMap").toFile(), "test.yml");
+        EBMap ebMap = new EBMap();
+        UnicodeMap unicodeMap = new UnicodeMap(mapPath);
+        Appendix appendix = ebMap.constructData(unicodeMap);
+        ebMap.yamlFile = outPath;
+        ebMap.generateYaml(appendix);
     }
 
 }
